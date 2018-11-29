df2e915949b3e86f30e9c83955656b4a26543bcb
lcsoul <>
2018-11-29 15:26:09 +0800

    test

---
+++ tools/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tools'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
